610e53d9ed3be97f8e5db562c344db63245e7dec
leandrovsk <contas.center@gmail.com>
2022-09-26 02:13:44 -0300

    added register/login pages

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,1 +1,11 @@
-/* Desenvolva a lógica da página aqui */
+let registerLink = document.querySelector(".nav-link");
+let registerBtn = document.querySelector(".redirect-btn")
+
+
+registerLink.addEventListener('click', function() {
+   window.location.href = '/pages/register/index.html'
+})
+
+registerBtn.addEventListener('click', function() {
+   window.location.href = '/pages/register/index.html'
+})
---
+++ pages/register/index.js
@@ -0,0 +1,15 @@
+let loginLink = document.querySelector(".nav-link");
+let loginBtn = document.querySelector(".back-login-btn");
+let backBtn = document.querySelector(".go-back-btn");
+
+loginLink.addEventListener("click", function () {
+  window.location.href = "/pages/login/index.html";
+});
+
+loginBtn.addEventListener("click", function () {
+  window.location.href = "/pages/login/index.html";
+});
+
+backBtn.addEventListener("click", function () {
+  window.location.href = "/pages/login/index.html";
+});
